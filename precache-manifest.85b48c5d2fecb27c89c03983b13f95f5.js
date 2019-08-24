d7f4d6e86a8604c7685683b01d79f27aa7b3ed1e
Joe Hayden <eljoeyy@gmail.com>
2019-08-23 19:47:29 -0500

    Updates

---
+++ precache-manifest.85b48c5d2fecb27c89c03983b13f95f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95674de62df55115e6fa3acc91753647",
+    "url": "/weather-2/index.html"
+  },
+  {
+    "revision": "55a2b3689746855dfe97",
+    "url": "/weather-2/static/css/main.6728c3bd.chunk.css"
+  },
+  {
+    "revision": "1fbc7e5323e55c64a3a2",
+    "url": "/weather-2/static/js/2.505c7a3a.chunk.js"
+  },
+  {
+    "revision": "55a2b3689746855dfe97",
+    "url": "/weather-2/static/js/main.974c2b3f.chunk.js"
+  },
+  {
+    "revision": "2fc071da122163f27d68",
+    "url": "/weather-2/static/js/runtime~main.bd19fbbb.js"
+  }
+]);
